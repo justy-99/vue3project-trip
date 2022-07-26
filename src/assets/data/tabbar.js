af67703661868c016ac2eb75a5d6d070aa23d0e7
suzhenyi <suzhenyi1999@163.com>
2022-07-27 00:23:21 +0800

    tabbar封装

---
+++ src/assets/data/tabbar.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    title: "首页",
+    path: "/home",
+    img: "tabbar/tab_home.png",
+    avtiveImg: "tabbar/tab_home_active.png"
+  },
+  {
+    title: "收藏",
+    path: "/favor",
+    img: "tabbar/tab_favor.png",
+    avtiveImg: "tabbar/tab_favor_active.png"
+  },
+  {
+    title: "订单",
+    path: "/order",
+    img: "tabbar/tab_order.png",
+    avtiveImg: "tabbar/tab_order_active.png"
+  },
+  {
+    title: "消息",
+    path: "/message",
+    img: "tabbar/tab_message.png",
+    avtiveImg: "tabbar/tab_message.png"
+  },
+]
---
+++ src/components/tab-bar/tab-bar-item.vue
@@ -0,0 +1,53 @@
+<script setup>
+  import { computed } from 'vue'
+  import { useRouter,useRoute } from 'vue-router'
+  import { getAssetURL } from '@/utils/load_assets'
+  
+  const router = useRouter()
+  const route = useRoute()
+  const props = defineProps({
+    itemData: {
+      type: Object,
+      default: ()=>({})
+    }
+  })
+  // 当前item是否处于激活状态
+  let activated = computed(() => route.path.indexOf(props.itemData.path) !== -1)
+  // 点击切换路由 
+  const clickTab = (path) => router.push(path)
+</script>
+
+<template>
+  <div class="tab-bar-item" @click="clickTab(itemData.path)">
+    <img v-if="activated" :src="getAssetURL(itemData.avtiveImg)" alt="">
+    <img v-else :src="getAssetURL(itemData.img)" alt="">
+    <span :class="{active : activated}" class="text">{{itemData.title}}</span>
+  </div>
+</template>
+
+
+<style lang="less" scoped>
+  .tab-bar-item {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-weight: 700;
+    
+    img {
+      width: 36px;
+      height: 28px;
+    }
+    & .text{
+      margin-top: 2px;
+      font-size: 12px;
+    }
+
+    & .active {
+      color: #f33;
+    }
+
+  }
+
+</style>
