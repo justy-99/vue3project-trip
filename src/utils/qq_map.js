e3f470d97fd02f28f0b11f1cdebda9ce5947dd55
suzhenyi <suzhenyi1999@163.com>
2022-08-06 23:24:59 +0800

    定位获取loading

--- src/utils/qq_map.js
+++ src/utils/qq_map.js
@@ -1,5 +1,8 @@
 import laodJs from './dynamic_load_js'
 // 腾讯位置服务
+import useMainStore from '@/stores/modules/main'
+const mainStore = useMainStore()
+
 const qqmapapi = 'https://mapapi.qq.com/web/mapComponents/geoLocation/v/geolocation.min.js'
 const KEY = '2IVBZ-XMMLW-IT3RI-OWPFQ-DFUEK-NQBEO'
 const APPNAME = '地图学习'
@@ -10,10 +13,13 @@ const getLoaction = () => {
       const qqgeolocation = new qq.maps.Geolocation(KEY, APPNAME);
       resolve(qqgeolocation)
     } else {
+      mainStore.isLoading = true
       laodJs(qqmapapi).then(function () {
+        mainStore.isLoading = false
         const qqgeolocation = new qq.maps.Geolocation(KEY, APPNAME);
         resolve(qqgeolocation)
       }).catch((err) => {
+        mainStore.isLoading = false
         reject(err)
       })
     }
